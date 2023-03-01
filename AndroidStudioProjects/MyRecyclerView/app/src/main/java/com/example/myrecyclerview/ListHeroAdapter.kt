44cf8d66ebfd18a286d1ff4711dd2aaa99f169fc
Saddam Azy <myxzlpltk@gmail.com>
2023-03-01 10:12:34 +0700

    view binding

--- AndroidStudioProjects/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
+++ AndroidStudioProjects/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
@@ -1,12 +1,10 @@
 package com.example.myrecyclerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myrecyclerview.databinding.ItemRowHeroBinding
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
@@ -18,9 +16,9 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
-        return ListViewHolder(view)
+        val binding =
+            ItemRowHeroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun getItemCount(): Int = listHero.size
@@ -28,19 +26,15 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
 
-        Glide.with(holder.itemView.context).load(photo).into(holder.imgPhoto)
-        holder.tvName.text = name
-        holder.tvDescription.text = description
-        holder.itemView.setOnClickListener {
+        Glide.with(holder.itemView.context).load(photo).into(holder.binding.imgItemPhoto)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemDescription.text = description
+        holder.binding.cardView.setOnClickListener {
             onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
         }
     }
 
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
-    }
+    class ListViewHolder(var binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root)
 
     interface OnItemClickCallback {
         fun onItemClicked(data: Hero)
