d06bc25ca81dc7e1cace8f6307b85ee09f7b7cba
Saddam Azy <myxzlpltk@gmail.com>
2023-03-01 09:23:51 +0700

    view binding

--- AndroidStudioProjects/VolumeCalc/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidStudioProjects/VolumeCalc/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,55 +2,45 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private lateinit var editHeight: EditText
-    private lateinit var editLength: EditText
-    private lateinit var editWidth: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var textResult: TextView
 
     companion object {
         private const val STATE_RESULT = "state_result"
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        editHeight = findViewById(R.id.editTextHeight)
-        editLength = findViewById(R.id.editTextLength)
-        editWidth = findViewById(R.id.editTextWidth)
-        btnCalculate = findViewById(R.id.buttonCalculate)
-        textResult = findViewById(R.id.textViewResult)
-
-        btnCalculate.setOnClickListener(this)
+        binding.buttonCalculate.setOnClickListener(this)
 
         if (savedInstanceState != null) {
-            textResult.text = savedInstanceState.getString(STATE_RESULT)
+            binding.textResult.text = savedInstanceState.getString(STATE_RESULT)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, textResult.text.toString())
+        outState.putString(STATE_RESULT, binding.textResult.text.toString())
     }
 
     override fun onClick(v: View?) {
-        if (v?.id == R.id.buttonCalculate) {
-            if (!validate(editLength) || !validate(editWidth) || !validate(editHeight)) return
+        if (v?.id == R.id.button_calculate) {
+            if (!validate(binding.editLength) || !validate(binding.editWidth) || !validate(binding.editHeight)) return
 
-            val inputHeight = editHeight.text.toString().trim().toDouble()
-            val inputLength = editLength.text.toString().trim().toDouble()
-            val inputWidth = editWidth.text.toString().trim().toDouble()
+            val inputHeight = binding.editHeight.text.toString().trim().toDouble()
+            val inputLength = binding.editLength.text.toString().trim().toDouble()
+            val inputWidth = binding.editWidth.text.toString().trim().toDouble()
 
             val volume = inputHeight * inputLength * inputWidth
-            textResult.text = volume.toString()
+            binding.textResult.text = volume.toString()
         }
     }
 
