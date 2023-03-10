9305c7f5665bd5e80ea82bbbdc1fee6db6b381e0
Saddam Azy <myxzlpltk@gmail.com>
2023-03-10 18:50:26 +0700

    with lifecycle and coroutines

--- AndroidStudioProjects/MyBackgroundThread/app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
+++ AndroidStudioProjects/MyBackgroundThread/app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.mybackgroundthread
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -18,28 +22,21 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-
         btnStart.setOnClickListener {
             btnStart.isEnabled = false
-            executor.execute {
-                try {
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
-                        handler.post {
-                            if (percentage == 100) tvStatus.setText(R.string.task_completed)
-                            else tvStatus.text =
-                                String.format(getString(R.string.compressing), percentage)
-                        }
+            lifecycleScope.launch(Dispatchers.Default) {
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        if (percentage == 100) tvStatus.setText(R.string.task_completed)
+                        else tvStatus.text =
+                            String.format(getString(R.string.compressing), percentage)
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                } finally {
-                    handler.post {
-                        btnStart.isEnabled = true
-                    }
+                }
+
+                withContext(Dispatchers.Main) {
+                    btnStart.isEnabled = true
                 }
             }
         }
