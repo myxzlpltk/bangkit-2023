044fa5147e63c98269cae05e63087d54ad9aa5be
Saddam Azy <myxzlpltk@gmail.com>
2023-03-10 13:15:02 +0700

    Codelab Tab Layout With One Fragment

--- AndroidStudioProjects/MyTabLayout/app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
+++ AndroidStudioProjects/MyTabLayout/app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
@@ -1,16 +1,18 @@
 package com.example.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> HomeFragment()
-            else -> ProfileFragment()
+        return HomeFragment().apply {
+            arguments = Bundle().apply {
+                putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+            }
         }
     }
 }
