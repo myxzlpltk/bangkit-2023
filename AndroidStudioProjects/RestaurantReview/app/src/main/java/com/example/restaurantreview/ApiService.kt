c2e40a802bfcaa47f8beffe48c1a044d7333cf90
Saddam Azy <myxzlpltk@gmail.com>
2023-03-11 19:26:43 +0700

    Codelab Mengambil Data Review Restoran

---
+++ AndroidStudioProjects/RestaurantReview/app/src/main/java/com/example/restaurantreview/ApiService.kt
@@ -0,0 +1,9 @@
+package com.example.restaurantreview
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(@Path("id") id: String): Call<RestaurantResponse>
+}
---
+++ AndroidStudioProjects/RestaurantReview/app/src/main/java/com/example/restaurantreview/ReviewAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.restaurantreview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewAdapter(private val listReview: List<String>) :
+    RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvItem: TextView = view.findViewById(R.id.tvItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_review, parent, false)
+    )
+
+    override fun getItemCount(): Int = listReview.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvItem.text = listReview[position]
+    }
+}
