ba796b8948a5fc599a4e18d6c496c39e455f782c
Saddam Azy <myxzlpltk@gmail.com>
2023-03-17 11:21:03 +0700

    Latihan Notification dengan PendingIntent

--- AndroidStudioProjects/SimpleNotif/app/src/main/java/com/dicoding/simplenotif/MainActivity.kt
+++ AndroidStudioProjects/SimpleNotif/app/src/main/java/com/dicoding/simplenotif/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dicoding.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -25,9 +28,13 @@ class MainActivity : AppCompatActivity() {
 
     //aksi untuk onClick pada button
     fun sendNotification(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         val mNotificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.baseline_notifications_white_48)
             .setLargeIcon(
                 BitmapFactory.decodeResource(
