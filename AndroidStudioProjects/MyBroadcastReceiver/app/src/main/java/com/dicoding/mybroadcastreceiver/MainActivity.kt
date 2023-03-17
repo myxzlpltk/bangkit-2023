ec79fcdfff470a8ab1ff971b4102c8f2da65076d
Saddam Azy <myxzlpltk@gmail.com>
2023-03-17 09:43:43 +0700

    Latihan BroadcastReceiver dari Custom Event

--- AndroidStudioProjects/MyBroadcastReceiver/app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
+++ AndroidStudioProjects/MyBroadcastReceiver/app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.dicoding.mybroadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
@@ -9,16 +14,23 @@ import com.dicoding.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
+        const val SMS_REQUEST_CODE = 101
+    }
+
+    private lateinit var downloadReceiver: BroadcastReceiver
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-    var requestPermissionLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
-        if (isGranted) {
-            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+    private var requestPermissionLauncher =
+        registerForActivityResult(RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+            }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,16 +38,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         binding.btnPermission.setOnClickListener(this)
+        binding.btnDownload.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                Log.d(DownloadService.TAG, "Download Selesai")
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(view: View) {
-        if (view.id == binding.btnPermission.id) {
-            requestPermissionLauncher.launch(android.Manifest.permission.RECEIVE_SMS)
+        when (view.id) {
+            R.id.btn_permission -> requestPermissionLauncher.launch(android.Manifest.permission.RECEIVE_SMS)
+            R.id.btn_download -> {
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(downloadReceiver)
         _binding = null
     }
 }
