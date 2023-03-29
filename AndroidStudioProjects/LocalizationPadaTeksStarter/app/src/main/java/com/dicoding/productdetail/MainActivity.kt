c722c213f4adc8e1264db1b6e89995e30021dfc8
Saddam Azy <myxzlpltk@gmail.com>
2023-03-29 14:57:58 +0700

    Latihan Localization pada Teks untuk Mendukung Multi-Language

---
+++ AndroidStudioProjects/LocalizationPadaTeksStarter/app/src/main/java/com/dicoding/productdetail/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.dicoding.productdetail
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.productdetail.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION") if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.settingsButton.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+    }
+}
