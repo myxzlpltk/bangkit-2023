5dfb1002d523c5673c242d7574eb4823181861ae
Saddam Azy <myxzlpltk@gmail.com>
2023-04-08 04:28:46 +0700

    Latihan Paging 3 dengan RemoteMediator

--- AndroidStudioProjects/LatihanPaging3/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
+++ AndroidStudioProjects/LatihanPaging3/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
@@ -1,10 +1,7 @@
 package com.dicoding.myunlimitedquotes.data
 
 import androidx.lifecycle.LiveData
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.liveData
+import androidx.paging.*
 import com.dicoding.myunlimitedquotes.database.QuoteDatabase
 import com.dicoding.myunlimitedquotes.network.ApiService
 import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
@@ -13,10 +10,12 @@ class QuoteRepository(
     private val quoteDatabase: QuoteDatabase,
     private val apiService: ApiService,
 ) {
+    @OptIn(ExperimentalPagingApi::class)
     fun getQuote(): LiveData<PagingData<QuoteResponseItem>> {
         return Pager(
-            config = PagingConfig(pageSize = 5),
-            pagingSourceFactory = { QuotePagingSource(apiService) },
+            config = PagingConfig(pageSize = 25),
+            remoteMediator = QuoteRemoteMediator(quoteDatabase, apiService),
+            pagingSourceFactory = { quoteDatabase.quoteDao().getAllQuote() },
         ).liveData
     }
 }
