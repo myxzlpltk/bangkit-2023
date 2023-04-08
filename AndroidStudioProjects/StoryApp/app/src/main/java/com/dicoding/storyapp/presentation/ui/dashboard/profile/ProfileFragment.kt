6e30bdc015c8de06e53d8eef4358c546c8a69978
Saddam Azy <myxzlpltk@gmail.com>
2023-04-08 17:45:39 +0700

    profile fragment and add selector bottom navigation icon

--- AndroidStudioProjects/StoryApp/app/src/main/java/com/dicoding/storyapp/presentation/ui/dashboard/profile/ProfileFragment.kt
+++ AndroidStudioProjects/StoryApp/app/src/main/java/com/dicoding/storyapp/presentation/ui/dashboard/profile/ProfileFragment.kt
@@ -1,26 +1,54 @@
 package com.dicoding.storyapp.presentation.ui.dashboard.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.dicoding.storyapp.R
+import com.dicoding.storyapp.databinding.FragmentProfileBinding
+import com.dicoding.storyapp.presentation.ui.sign_in.SignInActivity
 
 class ProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
     private val viewModel by activityViewModels<ProfileViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+        setupActions()
+    }
+
+    private fun setupView() {
+        viewModel.userLiveData.observe(viewLifecycleOwner) { user ->
+            binding.tvName.text = user.name
+        }
+    }
+
+    private fun setupActions() {
+        binding.signOut.setOnClickListener {
+            viewModel.logout()
+            startActivity(Intent(activity, SignInActivity::class.java))
+            requireActivity().finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- AndroidStudioProjects/StoryApp/app/src/main/java/com/dicoding/storyapp/presentation/ui/dashboard/profile/ProfileViewModel.kt
+++ AndroidStudioProjects/StoryApp/app/src/main/java/com/dicoding/storyapp/presentation/ui/dashboard/profile/ProfileViewModel.kt
@@ -1,14 +1,19 @@
 package com.dicoding.storyapp.presentation.ui.dashboard.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.dicoding.storyapp.data.preference.UserPreference
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(private val pref: UserPreference) : ViewModel() {
+
+    val userLiveData = pref.getUser().mapNotNull { it }.asLiveData()
+
     fun logout() {
         viewModelScope.launch {
             pref.logout()
