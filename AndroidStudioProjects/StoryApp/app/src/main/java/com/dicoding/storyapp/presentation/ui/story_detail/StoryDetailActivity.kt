2cc6f993f393f105781e76f607423af1fd7aba56
Saddam Azy <myxzlpltk@gmail.com>
2023-04-09 12:22:31 +0700

    add maps intent

--- AndroidStudioProjects/StoryApp/app/src/main/java/com/dicoding/storyapp/presentation/ui/story_detail/StoryDetailActivity.kt
+++ AndroidStudioProjects/StoryApp/app/src/main/java/com/dicoding/storyapp/presentation/ui/story_detail/StoryDetailActivity.kt
@@ -1,6 +1,9 @@
 package com.dicoding.storyapp.presentation.ui.story_detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.storyapp.data.entity.Story
 import com.dicoding.storyapp.databinding.ActivityStoryDetailBinding
@@ -43,6 +46,20 @@ class StoryDetailActivity : AppCompatActivity() {
         binding.tvDetailName.text = story.name
         binding.tvDetailDescription.text = story.description
         binding.tvDetailDate.text = story.createdAt.toLocaleFormat()
+
+        if (story.latitude != null && story.longitude != null) {
+            binding.fab.setOnClickListener {
+                val uriString = "geo:0,0?q=${story.name}@${story.latitude},${story.longitude},z=15"
+                val gmmIntentUri = Uri.parse(uriString)
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                if (mapIntent.resolveActivity(packageManager) != null) {
+                    startActivity(mapIntent)
+                }
+            }
+        } else {
+            binding.fab.visibility = View.GONE
+        }
     }
 
     private fun setupActions() {
