dcbf3c1728e97aa596a9d0a85cbf3756a59d29f8
Saddam Azy <myxzlpltk@gmail.com>
2023-04-17 03:47:41 +0700

    reset compose bullshit

--- AndroidStudioProjects/GithubUserCompose/app/src/main/java/com/example/githubusercompose/GithubUserApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.githubusercompose
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class GithubUserApplication : Application()
--- AndroidStudioProjects/GithubUserCompose/app/src/main/java/com/example/githubusercompose/config/GithubConfig.kt
+++
@@ -1,5 +0,0 @@
-package com.example.githubusercompose.config
-
-const val INITIAL_ID = 0
-const val INITIAL_PAGE_INDEX = 1
-const val PAGE_SIZE = 30
