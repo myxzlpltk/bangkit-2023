fab240f0044ec6f2dcd2b1b0ab2cc80ff8e0fe68
Saddam Azy <myxzlpltk@gmail.com>
2023-05-29 02:05:30 +0700

    Studi Kasus Dagger Hilt pada Aplikasi

--- AndroidStudioProjects/TourismAppCleanArchitecture/app/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
+++ AndroidStudioProjects/TourismAppCleanArchitecture/app/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
@@ -6,18 +6,26 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDao
 import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(context: Context) : TourismDatabase = Room.databaseBuilder(
-        context,
-        TourismDatabase::class.java, "Tourism.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): TourismDatabase {
+        return Room.databaseBuilder(
+            context,
+            TourismDatabase::class.java, "Tourism.db"
+        ).fallbackToDestructiveMigration().build()
+    }
 
     @Provides
-    fun provideTourismDao(database: TourismDatabase): TourismDao = database.tourismDao()
+    fun provideTourismDao(database: TourismDatabase): TourismDao {
+        return database.tourismDao()
+    }
 }
--- AndroidStudioProjects/TourismAppCleanArchitecture/app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteFragment.kt
+++ AndroidStudioProjects/TourismAppCleanArchitecture/app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteFragment.kt
@@ -1,34 +1,22 @@
 package com.dicoding.tourismapp.favorite
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.dicoding.tourismapp.MyApplication
 import com.dicoding.tourismapp.core.ui.TourismAdapter
-import com.dicoding.tourismapp.core.ui.ViewModelFactory
 import com.dicoding.tourismapp.databinding.FragmentFavoriteBinding
 import com.dicoding.tourismapp.detail.DetailTourismActivity
-import javax.inject.Inject
 
 class FavoriteFragment : Fragment() {
 
-    @Inject
-    lateinit var factory: ViewModelFactory
-    private val favoriteViewModel: FavoriteViewModel by viewModels { factory }
-
+    private val favoriteViewModel: FavoriteViewModel by activityViewModels()
     private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        (requireActivity().application as MyApplication).appComponent.inject(this)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
