360e23807bd5233749ea47de5a351673571e15d8
Saddam Azy <myxzlpltk@gmail.com>
2023-05-29 00:36:15 +0700

    Studi Kasus Koin pada Aplikasi

--- AndroidStudioProjects/TourismAppCleanArchitecture/app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
+++ AndroidStudioProjects/TourismAppCleanArchitecture/app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
-class TourismRepository private constructor(
+class TourismRepository(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource,
     private val appExecutors: AppExecutors
